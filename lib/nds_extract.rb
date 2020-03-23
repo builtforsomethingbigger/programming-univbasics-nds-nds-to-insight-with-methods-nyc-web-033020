c485f6c3aa1657ec33d15794054a3aa50d93e960
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-23 15:53:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,7 @@ pp directors_database
 def gross_for_director(director_data)
   index = 0
   total = 0
-  while index < director_data.length do
+  while index < director_data.size do
     total = director_data[:movies][index][:worldwide_gross]
     index += 1
   end
